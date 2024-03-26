1029bf0454f01dfc124a148b73dc3a73f472eb6a
wrule <1982775886@qq.com>
2024-03-26 15:46:43 +0800

    基本的http服务

--- src/dev.ts
+++ src/dev.ts
@@ -1,7 +1,19 @@
-import { hello } from '.';
+import Koa from 'koa';
+import Router from 'koa-router';
 
 function dev() {
-  hello();
+  const app = new Koa();
+  const router = new Router();
+  router.get('/1', (ctx, next) => {
+    ctx.body = '哈哈';
+  });
+  router.get('/2', (ctx, next) => {
+    ctx.body = '呼呼';
+  });
+  app
+    .use(router.routes())
+    .use(router.allowedMethods());
+  app.listen(3000);
 }
 
 dev();
