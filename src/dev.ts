6a9890c8fa2e49b18a1fe792acdc91f6800d9fbd
wrule <1982775886@qq.com>
2024-04-18 17:30:37 +0800

    提交

--- src/dev.ts
+++ src/dev.ts
@@ -5,9 +5,12 @@ function dev() {
   const app = new Koa();
   const router = new Router();
   router.get('/1', (ctx, next) => {
+    ctx.cookies.set('jimao', '1234');
+    console.log('哈哈');
     ctx.body = '哈哈';
   });
   router.get('/2', (ctx, next) => {
+    console.log('呼呼', ctx.cookies.get('jimao'));
     ctx.body = '呼呼';
   });
   app
